881e12f04b28d62a7f8cd3c66e60b089eda5f954
darthvader2 <kvarshith@outlook.com>
2021-06-16 23:18:19 +0200

    footer component and spa

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Foot = styled.div`
+width: 100%;
+height: 9rem;
+background-color: black;
+margin-top:80%;
+`;
+
+const Footer = () => {
+    return(
+        <Foot>
+            <a>Footer</a>
+        </Foot>
+    )
+}
+
+export default Footer
